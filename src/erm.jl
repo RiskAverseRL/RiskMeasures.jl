ee36b40a2ba33ff1888de78305c672ea524740b2
Gersi.doko <Gersi.doko@unh.edu>
2024-11-27 11:50:10 -0500

    remove mention of _e methods in risk measures

--- src/erm.jl
+++ src/erm.jl
@@ -17,23 +17,23 @@ More details: https://en.wikipedia.org/wiki/Entropic_risk_measure
 function ERM end
 
 function ERM(values::AbstractVector{<:Real}, pmf::AbstractVector{<:Real}, β::Real;
-             x̃min::Real = -Inf, check_inputs = true)
-    
+    x̃min::Real=-Inf, check_inputs=true)
+
     check_inputs && _check_pmf(values, pmf)
     β < zero(β) && _bad_risk("Risk level β must be non-negative.")
 
     if iszero(β)
-        return values' * pmf 
+        return values' * pmf
     elseif isinf(β) && β > zero(β)
-        return essinf_e(values, pmf; check_inputs = false).value
-    end 
+        return essinf(values, pmf; check_inputs=false).value
+    end
     # because entropic risk measure is translation equivariant, we can change values
     # so that it is positive. That change makes it less likely that it overflows
     x̃min = isfinite(x̃min) ? x̃min : minimum(values)
-    
+
     #@fastmath x̃min-one(β) / β*log(sum(pmf .* exp.(-β .* (values .- x̃min) ))) |> float
-    a = Iterators.map((x,p) -> (@fastmath p * exp(-β * (x - x̃min))), values, pmf) |> sum
-    @fastmath x̃min - one(β) / β * log(a) 
+    a = Iterators.map((x, p) -> (@fastmath p * exp(-β * (x - x̃min))), values, pmf) |> sum
+    @fastmath x̃min - one(β) / β * log(a)
 end
 
 ERM(x̃, β::Real; kwargs...) = ERM(rv2pmf(x̃)..., β; kwargs...)
@@ -58,17 +58,17 @@ The value β must be positive
 function softmin end
 
 function softmin(values::AbstractVector{<:Real}, pmf::AbstractVector{<:Real},
-                 β::Real; x̃min::Real = -Inf, check_inputs = true) 
-    
+    β::Real; x̃min::Real=-Inf, check_inputs=true)
+
     check_inputs && _check_pmf(values, pmf)
 
     if β > zero(β)
         # TODO: only needs to look at values with pos prob.
         # because softmin is translation invariant add the subtract the smallest value
         # ensures that values ≥ 0
-        if isfinite(x̃min) 
+        if isfinite(x̃min)
             np = similar(pmf)
-            np .= @fastmath pmf .* exp.(-β .* (values .- x̃min) )
+            np .= @fastmath pmf .* exp.(-β .* (values .- x̃min))
             np .= @fastmath inv(sum(np)) .* np
             mapreduce(isfinite, &, np) || error("Overflow, reduce β.")
             np
