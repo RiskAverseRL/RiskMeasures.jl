56b0e51ecae5ae8acd2feb025274abaa7b5e510d
Marek Petrik <mpetrik@cs.unh.edu>
2026-05-21 14:49:01 -0400

    streamlined ubsr and fixed tests

--- src/ubsr.jl
+++ src/ubsr.jl
@@ -1,22 +1,31 @@
 using Distributions
 
 """
-    UBSR(x, p, u, λ; z_min=-1e6, z_max=1e6, tol=1e-7)
+    UBSR(x̃, u, λ; ...)
 
-Compute the Utility Based Shortfall RiskMeasure (UBSR) for a 
-discrete random variable with reward vector `x`, probability vector `p`,
+Compute the Utility Based Shortfall RiskMeasure (UBSR) for a discrete random variable `x̃`,
 for a monotone function `u`, and risk-threshold `λ`.
+
+    UBSR(x, p, u, λ; ...)
+
+Compute the Utility Based Shortfall RiskMeasure (UBSR) for a discrete random variable with
+values `x`, and pmf `p`, for a monotone function `u`, and risk-threshold `λ`.
 
 ```math
 \\operatorname{UBSR(x, p, u, λ) =
-\\sup \\{z ∈ ℝ \\mid \\mathbb{E}[u(x - z)] ≥ λ \\} = \\sup \\{z ∈ ℝ \\mid g(z) ≥ λ \\}
+\\sup \\{z ∈ ℝ \\mid \\mathbb{E}[u(\\tilde{x} - z)] ≥ λ \\} = \\sup \\{z ∈ ℝ \\mid g(z) ≥ λ \\}
+```
+where
+```math
+g(z) := \\mathbb{E}[u(\\tilde{x} - z)]
 ```
 
-If `u` is NOT monotone (non-descreasing), the function return is undefined.
+If `u` is NOT monotone (non-decreasing), the `UBSR` return is undefined and may
+terminate with an error.
 
 # Keyword Arguments:
-- `z_min`, `z_max`: Bounds for the bisection search.
-- `tol`: Tolerance for convergence of the bisection method.
+- `zmin=-1e6`, `zmax-1e6`: Lower and upper bounds for the bisection search.
+- `tol=1e-7`: Tolerance for convergence of the bisection method.
 
 # Returns
 - A named tuple with the computed UBSR `value`.
@@ -24,36 +33,29 @@ If `u` is NOT monotone (non-descreasing), the function return is undefined.
 function UBSR end
 
 function UBSR(x::AbstractVector{<:Real}, p::AbstractVector{<:Real}, u::Function, λ::Real;
-              z_min=-1e6, z_max=1e6, tol=1e-6, check_inputs=true)
-
+              zmin::Float64=-1e6, zmax::Float64=1e6, tol::Float64=1e-6, check_inputs=true)
     check_inputs && _check_pmf(p)
+    zmin < zmax || error("zmin < zmax is violated")
     
-    ExU(z) = p' * u.(x .- z) # is non-increasing
-    
-    f_min = ExU(z_min) 
-    f_max = ExU(z_max) 
-    
-    f_min < λ && error("z_min is too high: E[u(x - z_min)] < λ.")
-    f_max > λ && error("z_max is too low: E[u(x - z_max)] > λ.")
+    g(z) = p' * u.(x .- z) # is non-increasing
 
-    f_min > f_max && error("Function u is not monotone.")
+    g(zmin) < λ && error("zmin is too high: E[u(x - z_min)] < λ.")
+    g(zmax) > λ && error("zmax is too low: E[u(x - z_max)] > λ.")
+    g(zmin) < g(zmax) && error("Function g is not monotone: $(g(zmin)) < $(g(gmax)).")
     
     # Bisection Method
-    low = z_min
-    high = z_max
-    while (high - low) > tol
-        z_mid = (low + high) / 2
-        u_mid = ExU(z_mid)
-        if u_mid ≥ λ
-            # u(x-z) is monotone in z => ExU ≥ λ
-            low = z_mid
-        elseif u_mid 
-        else
-            high = z_mid
+    while (zmax - zmin) > tol
+        zmid = (zmin + zmax) / 2
+        umid = g(zmid)
+
+        if umid ≥ λ
+            zmin = zmid
+        end
+        if umid ≤ λ
+            zmax = zmid
         end
     end
-
-    (value=(low + high) / 2,)
+    (value=(zmin + zmax) / 2,)
 end
 
 function UBSR(x̃, u, λ; kwargs...)
